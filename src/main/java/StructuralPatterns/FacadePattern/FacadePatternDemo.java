92b0597341dfea0ebebfd10dcd55abdd4893407d
281613520 <281613520@qq.com>
2017-07-20 23:14:08 +0800

    过滤器模式

---
+++ src/main/java/StructuralPatterns/FacadePattern/FacadePatternDemo.java
@@ -0,0 +1,14 @@
+package StructuralPatterns.FacadePattern;
+
+/**
+ * Created by Ankh on 2017/7/20.
+ */
+public class FacadePatternDemo {
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+}
