03864f944afd3ed1c64c0c2ae3f86514970ef871
281613520 <281613520@qq.com>
2017-07-24 20:06:38 +0800

    享元模式

---
+++ src/main/java/StructuralPatterns/FlyweightPattern/Circle.java
@@ -0,0 +1,53 @@
+package StructuralPatterns.FlyweightPattern;
+
+/**
+ * Created by Ankh on 2017/7/24.
+ */
+public class Circle implements Shape{
+    private int x;
+    private int y;
+    private String color;
+    private int radius;
+
+    @Override
+    public void draw() {
+        System.out.println("Circle: Draw() [Color : " + color
+                +", x : " + x +", y :" + y +", radius :" + radius);
+    }
+
+    public Circle(String color) {
+        this.color = color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
---
+++ src/main/java/StructuralPatterns/FlyweightPattern/Shape.java
@@ -0,0 +1,8 @@
+package StructuralPatterns.FlyweightPattern;
+
+/**
+ * Created by Ankh on 2017/7/24.
+ */
+public interface Shape {
+    void draw();
+}
